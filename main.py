9e4b660378fee8c22656a8ca5aa0dff078cf3359
aram20030905 <117174311+aram20030905@users.noreply.github.com>
2024-12-03 14:57:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -22,28 +22,44 @@ I hope one day you can visit, although the only way to get here now is {verb_end
 '''
 ]
 
-def hospital_story():
-    # Collect inputs
-    number = input("Enter a number: ")
-    time_measure = input("Enter a measure of time (e.g., days, hours): ")
-    transportation = input("Enter a mode of transportation: ")
-    adjective1 = input("Enter an adjective: ")
-    adjective2 = input("Enter another adjective: ")
-    noun1 = input("Enter a noun: ")
-    color = input("Enter a color: ")
-    body_part = input("Enter a part of the body: ")
-    verb = input("Enter a verb: ")
-    number2 = input("Enter another number: ")
-    noun2 = input("Enter a noun: ")
-    noun3 = input("Enter a noun: ")
-    body_part2 = input("Enter another part of the body: ")
-    verb2 = input("Enter a verb: ")
-    noun4 = input("Enter a noun: ")
-    adjective3 = input("Enter an adjective: ")
-    silly_word = input("Enter a silly word: ")
-    noun5 = input("Enter a noun: ")
+def get_int_input(prompt, error_message="Invalid input, please enter a valid number:"):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print(error_message)
 
-    # Create and print the story
+def get_non_empty_input(prompt, error_message="Input cannot be empty. Please try again: "):
+    while True:
+        user_input = input(prompt)
+        if user_input.strip():
+            return user_input
+        else:
+            print(error_message)
+
+
+def hospital_story():
+    
+    number = get_int_input("Enter a number: ")
+    time_measure = get_non_empty_input("Enter a measure of time (e.g., days, hours): ")
+    transportation = get_non_empty_input("Enter a mode of transportation: ")
+    adjective1 = get_non_empty_input("Enter an adjective: ")
+    adjective2 = get_non_empty_input("Enter another adjective: ")
+    noun1 = get_non_empty_input("Enter a noun: ")
+    color = get_non_empty_input("Enter a color: ")
+    body_part = get_non_empty_input("Enter a part of the body: ")
+    verb = get_non_empty_input("Enter a verb: ")
+    number2 = get_int_input("Enter another number: ")
+    noun2 = get_non_empty_input("Enter a noun: ")
+    noun3 = get_non_empty_input("Enter a noun: ")
+    body_part2 = get_non_empty_input("Enter another part of the body: ")
+    verb2 = get_non_empty_input("Enter a verb: ")
+    noun4 = get_non_empty_input("Enter a noun: ")
+    adjective3 = get_non_empty_input("Enter an adjective: ")
+    silly_word = get_non_empty_input("Enter a silly word: ")
+    noun5 = get_non_empty_input("Enter a noun: ")
+
+  
     story = template[0].format(
         number=number, time_measure=time_measure, transportation=transportation, adjective1=adjective1,
         adjective2=adjective2, noun1=noun1, color=color, body_part=body_part, verb=verb, number2=number2,
@@ -53,26 +69,26 @@ def hospital_story():
     print(story)
 
 def camping_story():
-    # Collect inputs
-    person_name = input("Enter a person's name: ")
-    noun1 = input("Enter a noun (e.g., stick, map): ")
-    feeling1 = input("Enter an adjective (feeling) for excitement: ")
-    verb1 = input("Enter a verb (action): ")
-    feeling2 = input("Enter an adjective (feeling) for concern: ")
-    animal = input("Enter an animal: ")
-    verb2 = input("Enter a verb (action): ")
-    color = input("Enter a color: ")
-    action_verb = input("Enter a verb ending in 'ing' (e.g., swimming): ")
-    adverb = input("Enter an adverb (ending in -ly): ")
-    number = input("Enter a number: ")
-    time_measure = input("Enter a measure of time (e.g., days, hours): ")
-    color_animal = input("Enter a color: ")
-    animal2 = input("Enter an animal: ")
-    number3 = input("Enter a number: ")
-    silly_word = input("Enter a silly word: ")
-    noun2 = input("Enter a noun (e.g., marshmallow): ")
+  
+    person_name = get_non_empty_input("Enter a person's name: ")
+    noun1 = get_non_empty_input("Enter a noun (e.g., stick, map): ")
+    feeling1 = get_non_empty_input("Enter an adjective (feeling) for excitement: ")
+    verb1 = get_non_empty_input("Enter a verb (action): ")
+    feeling2 = get_non_empty_input("Enter an adjective (feeling) for concern: ")
+    animal = get_non_empty_input("Enter an animal: ")
+    verb2 = get_non_empty_input("Enter a verb (action): ")
+    color = get_non_empty_input("Enter a color: ")
+    action_verb = get_non_empty_input("Enter a verb ending in 'ing' (e.g., swimming): ")
+    adverb = get_non_empty_input("Enter an adverb (ending in -ly): ")
+    number = get_int_input("Enter a number: ")
+    time_measure = get_non_empty_input("Enter a measure of time (e.g., days, hours): ")
+    color_animal = get_non_empty_input("Enter a color: ")
+    animal2 = get_non_empty_input("Enter an animal: ")
+    number3 = get_int_input("Enter a number: ")
+    silly_word = get_non_empty_input("Enter a silly word: ")
+    noun2 = get_non_empty_input("Enter a noun (e.g., marshmallow): ")
 
-    # Create and print the story
+
     story = template[1].format(
         person_name=person_name, noun1=noun1, feeling1=feeling1, verb1=verb1,
         feeling2=feeling2, animal=animal, verb2=verb2, color=color,
@@ -83,29 +99,27 @@ def camping_story():
     print(story)
 
 def enchanted_castle_story():
-    # Collect inputs
-    person_name = input("Enter a person's name: ")
-    adjective1 = input("Enter an adjective: ")
-    color_animal = input("Enter a color: ")
-    animal = input("Enter an animal: ")
-    place = input("Enter a place (e.g., forest, mountain): ")
-    adjective2 = input("Enter an adjective: ")
-    magical_creature1 = input("Enter a magical creature (plural): ")
-    adjective3 = input("Enter another adjective: ")
-    magical_creature2 = input("Enter another magical creature (plural): ")
-    room = input("Enter a room in a house: ")
-    noun1 = input("Enter a noun: ")
-    noun2 = input("Enter a noun: ")
-    noun3 = input("Enter a plural noun: ")
-    adjective4 = input("Enter an adjective: ")
-    noun4 = input("Enter a plural noun: ")
-    number = input("Enter a number: ")
-    time_measure = input("Enter a measure of time (e.g., days, weeks): ")
-    verb_ending = input("Enter a verb ending in 'ing': ")
-    adjective5 = input("Enter an adjective: ")
-    noun5 = input("Enter a noun: ")
 
-    # Create and print the story
+    person_name = get_non_empty_input("Enter a person's name: ")
+    adjective1 = get_non_empty_input("Enter an adjective: ")
+    color_animal = get_non_empty_input("Enter a color: ")
+    animal = get_non_empty_input("Enter an animal: ")
+    place = get_non_empty_input("Enter a place (e.g., forest, mountain): ")
+    adjective2 = get_non_empty_input("Enter an adjective: ")
+    magical_creature1 = get_non_empty_input("Enter a magical creature (plural): ")
+    adjective3 = get_non_empty_input("Enter another adjective: ")
+    magical_creature2 = get_non_empty_input("Enter another magical creature (plural): ")
+    room = get_non_empty_input("Enter a room in a house: ")
+    noun1 = get_non_empty_input("Enter a noun: ")
+    noun2 = get_non_empty_input("Enter a noun: ")
+    noun3 = get_non_empty_input("Enter a plural noun: ")
+    adjective4 = get_non_empty_input("Enter an adjective: ")
+    noun4 = get_non_empty_input("Enter a plural noun: ")
+    number = get_int_input("Enter a number: ")
+    time_measure = get_non_empty_input("Enter a measure of time (e.g., days, weeks): ")
+    verb_ending = get_non_empty_input("Enter a verb ending in 'ing': ")
+    adjective5 = get_non_empty_input("Enter an adjective: ")
+    noun5 = get_non_empty_input("Enter a noun: ")
     story = template[2].format(
         person_name=person_name, adjective1=adjective1, color_animal=color_animal,
         animal=animal, place=place, adjective2=adjective2, magical_creature1=magical_creature1,
@@ -116,7 +130,7 @@ def enchanted_castle_story():
     )
     print(story)
 
-# Main logic
+
 chooseNumber = int(input("Enter a template number (1, 2, or 3): "))
 
 if chooseNumber == 1:
